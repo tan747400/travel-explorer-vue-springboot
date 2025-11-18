8fbf632dfca8b264d7664c981aca291fdc61d61e
Thanisorn <thanisorn.sir@gmail.com>
2025-11-18 13:47:51 +0700

    Initial commit: setup monorepo with client & server

---
+++ server/src/test/java/com/travel/explorer/server/TravelExplorerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.explorer.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelExplorerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
